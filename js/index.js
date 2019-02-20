18bcf1d0a3a10850a24ae5e41e54cbe7bafd4779
NickolasBenakis <nickolasbenele@gmail.com>
2019-02-21 00:15:50 +0200

    Add search filter functionality

---
+++ js/index.js
@@ -0,0 +1,44 @@
+
+
+//let city = document.getElementById('city').value;
+//city.addEventListener("city", getData(city));
+
+// function getData() {
+//     // console.log(city);
+//     // let city = document.getElementById('input_city').value;
+//     let city = document.querySelector('input').value;
+//     console.log(city);
+//     if (city === "Athens") {
+//         const http = new XMLHttpRequest();
+//         const apiKey = 'c7eb6d72320feed6f49470cab2537dd6';
+//         //const url = `api.openweathermap.org/data/2.5/weather?q=London`;
+//         const url2 = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=c7eb6d72320feed6f49470cab2537dd6';
+//         http.open("GET", url2);
+//         http.send();
+//         http.onreadystatechange = (e) => {
+//             console.log(http.responseText);
+//             console.log(e);
+//         }
+//     }
+
+// }
+
+
+function myFunction(){
+
+
+const list = document.getElementById('list-cities');
+const input = document.getElementById('input_city');
+const filter = input.value.toLowerCase();
+const cities = document.getElementsByTagName('li')
+// document.getElementsByTagName('li')[0].innerText; paizei ayti i malakia
+
+for (let i = 0; i < cities.length; i++) {
+    const title = cities[i].innerText;
+    if (title.toLowerCase().indexOf(filter) != -1) {
+        cities[i].style.display = 'block';
+    } else {
+        cities[i].style.display = 'none';
+        }
+    }
+}
