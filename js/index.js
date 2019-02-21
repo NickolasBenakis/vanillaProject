553225ef9165d4dd3f5b0cc1a0c9b635e2dec3aa
NickolasBenakis <nickolasbenele@gmail.com>
2019-02-21 12:54:39 +0200

    Add modal, clean code

--- js/index.js
+++ js/index.js
@@ -30,23 +30,71 @@ function filterCity() {
     const input = document.getElementById('input_city');
     const filter = input.value.toLowerCase();
     const cities = document.getElementsByTagName('li')
-    // document.getElementsByTagName('li')[0].innerText; paizei ayti i malakia
 
     for (let i = 0; i < cities.length; i++) {
         const title = cities[i].innerText;
-        if (filter == "" || filter == null) {
+        if (filter == "" || filter == undefined) {
+            cities[i].style.display = 'block';
+            cities[i].style.cursor = 'default';
             cities[i].style.animation = '.5s alternate ease-out';
             cities[i].style.animationPlayState = "paused";
         }
         else if (title.toLowerCase().indexOf(filter) != -1) {
             cities[i].style.display = 'block';
             cities[i].style.cursor = 'pointer';
-            cities[i].style.animation = 'bounce .5s alternate infinite ease-out';
+            cities[i].style.animation = 'bounce .8s alternate infinite ease-out';
+            if (filter.toLowerCase() === title.toLowerCase()) {
+                getCity(cities[i]);
+
+            }
 
         }
         else {
             cities[i].style.display = 'none';
         }
     }
+}
 
+// function getMetrics() {
+//     const cities = document.getElementsByTagName('li');
+//     Array.from(cities).forEach(element => {
+//         if (element.style.display = 'block') {
+//             console.log(element);
+
+//         }
+//     });
+// }
+
+function triggerModal(mycity,cityName) {
+    const dialog = document.getElementById('favDialog');
+    const cancelButton = document.getElementById('return');
+    mycity.addEventListener('click', () => {
+        dialog.showModal();
+        getMetrics(cityName);
+    });
+    cancelButton.addEventListener('click', () => {
+        dialog.close();
+    });
+}
+
+
+function getCity(mycity) {
+    const cityName = mycity.innerText;
+    console.log(cityName);
+    triggerModal(mycity,cityName);
+}
+
+
+function getMetrics(city) {
+    console.log("fernw kairo");
+    const http = new XMLHttpRequest();
+    const apiKey = 'c7eb6d72320feed6f49470cab2537dd6';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    http.open("GET", url);
+    http.send();
+    http.onreadystatechange = (e) => {
+        if (http.readyState == 4 && http.status == 200) {
+            console.log(http.responseText);
+        }
+    }
 }
