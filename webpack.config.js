a66dc3a52614abe0f7599bf80b9a8da23efb7d9c
NickolasBenakis <nickolasbenele@gmail.com>
2019-02-23 13:24:45 +0200

    Modify project structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 let path = require('path');
 let webpack = require('webpack');
 let ExtractTextPlugin = require('extract-text-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
 
 let extractPlugin = new ExtractTextPlugin({
     filename: 'main.css'
 });
 
 module.exports = {
-    entry: './js/index.js',
+    entry: './src/js/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
@@ -29,7 +30,7 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: extractPlugin.extract({
-                    use: ['css-loader','sass-loader']
+                    use: ['css-loader', 'sass-loader']
                 })
             }
         ]
